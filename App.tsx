7eedbf6b4dc7c04a679755d32311685ea9d6533c
GuFvr <gustavo.vfavero@gmail.com>
2024-03-26 22:22:55 -0300

    feat: Integrate Google Fonts into the application
    Added Google Fonts Roboto_400Regular and Roboto_700Bold using '@expo-google-fonts/roboto' package. Utilized the useFonts hook to load the fonts asynchronously. Conditionally render the Groups component when fonts are loaded, otherwise display an ActivityIndicator.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
+import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from './src/theme';
 
@@ -6,9 +8,11 @@ import theme from './src/theme';
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,29 +1,29 @@
 export default {
-  colors: {
-    white: '#FFFFFF',
+  COLORS: {
+    WHITE: '#FFFFFF',
 
-    green_700: '#00875F',
-    green_500: '#00B37E',
+    GREEN_700: '#00875F',
+    GREEN_500: '#00B37E',
 
-    red: '#F75A68',
-    dark_red: '#AA2834',
+    RED: '#F75A68',
+    RED_DARK: '#AA2834',
 
-    gray_700: '#121214',
-    gray_600: '#202024',
-    gray_500: '#29292E',
-    gray_400: '#323238',
-    gray_300: '#7C7C8A',
-    gray_200: '#C4C4CC',
-    gray_100: '#E1E1E6'
+    GRAY_700: '#121214',
+    GRAY_600: '#202024',
+    GRAY_500: '#29292E',
+    GRAY_400: '#323238',
+    GRAY_300: '#7C7C8A',
+    GRAY_200: '#C4C4CC',
+    GRAY_100: '#E1E1E6'
   },
-  font_family: {
-    regular: 'Roboto_400Regular',
-    bold: 'Roboto_700Bold'
+  FONT_FAMILY: {
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold'
   },
-  font_size: {
-    sm: 14,
-    md: 16,
-    lg: 18,
-    xl: 24
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24
   }
-};
+}
