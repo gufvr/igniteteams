6055a16ac6a9151936378d52ef2b2ade1c80adba
GuFvr <gustavo.vfavero@gmail.com>
2024-04-09 22:57:51 -0300

    feat: Implement Input component
    Created an Input component that wraps a TextInput from react-native. Styled the input container with custom styling for font, color, background, border radius, and padding.

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Highlight } from '@components/Highlight';
 import { Container, Content, Icon } from './styles';
 
 import { Header } from '@components/Header';
+import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function NewGroup() {
@@ -16,10 +17,11 @@ export function NewGroup() {
           subtitle='crie a turma adicionar as pessoas'
         />
 
-        
+        <Input />
 
         <Button 
           title='Criar'
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
