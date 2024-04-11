a53f33d2d3702862dd3be26ad454eddf21afe249
GuFvr <gustavo.vfavero@gmail.com>
2024-04-10 23:25:42 -0300

    feat: Implement dynamic ButtonIcon component
    Created the ButtonIcon component using Expo's "MaterialIcons" native library, allowing dynamic rendering of icons based on provided props. The component adjusts its style and icon color according to the specified type.

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <Players /> : <Loading /> }
     </ThemeProvider>
   );
 }
