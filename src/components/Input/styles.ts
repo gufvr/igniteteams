9d96d7369a3120cdcece7ec17c24f8772d1f570c
GuFvr <gustavo.vfavero@gmail.com>
2024-04-10 22:06:07 -0300

    refactor: CSS refactoring using CSS Helper
    Refactored the CSS styles throughout the app using the CSS Helper from styled-components/native. Updated color, font family, and font size properties for better consistency and maintainability.

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
   flex: 1;
@@ -7,12 +7,13 @@ export const Container = styled(TextInput)`
   min-height: 56px;
   max-height: 56px;
 
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  ${({ theme }) => css`
+  color: ${theme.COLORS.WHITE};
+  background-color: ${theme.COLORS.GRAY_700};
 
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  font-size: ${theme.FONT_SIZE.MD}px;  
+  `};
 
   border-radius: 6px;
   padding: 16px;
