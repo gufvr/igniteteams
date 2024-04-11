19040109b1c9c209f8bbadf66f52382f5e1f0b57
GuFvr <gustavo.vfavero@gmail.com>
2024-04-10 22:28:34 -0300

    feat: Implement ListEmpty component
    Added the ListEmpty component to display a message when the list is empty. The component dynamically adjusts its content container style and shows a customizable message.

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <NewGroup /> : <Loading /> }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,9 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+
+
 import { Button } from '@components/Button';
 
 import { Container } from './styles'
@@ -25,6 +28,13 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard 
             title={item} 
+          />
+        )}
+
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?'
           />
         )}
       />
