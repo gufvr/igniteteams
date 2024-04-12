f9172a2ec94ead8965de6943bc9be0c8f76bd033
GuFvr <gustavo.vfavero@gmail.com>
2024-04-11 22:51:59 -0300

    feat: Implement filter list and player count
    Added functionality to list filters for selecting teams and display the number of players in the selected team. The Filter component dynamically highlights the selected team, and the player count updates accordingly.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,10 +31,26 @@ export function Players() {
           icon='add'
         />
       </Form>
-      < Filter
-        title='Time A'
-        isActive
-      />
+
+      <HeaderList>
+        <FlatList 
+          data={['TIME A', 'TIME B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            < Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}         
+        </NumbersOfPlayers>
+      </HeaderList>
+
     </Container>
   );
 }
